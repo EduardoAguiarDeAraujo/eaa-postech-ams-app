cc2bc820e42fd0166c220f4e6dae9797fdd02b2a
Eduardo Araujo <araujo.ea@gmail.com>
2025-11-04 21:56:14 -0300

    AjUpdate postman collections

--- notification/src/main/java/br/eng/eaa/notifications/listener/AppointmentListener.java
+++ notification/src/main/java/br/eng/eaa/notifications/listener/AppointmentListener.java
@@ -10,7 +10,6 @@ public class AppointmentListener {
 
     @RabbitListener(queues = RabbitMQConfig.APPOINTMENT_QUEUE)
     public void receiveMessage(String message) {
-        // Simula envio de lembrete (em produção, integrar com e-mail/SMS)
         System.out.println("Recebida mensagem: " + message);
         System.out.println("Enviando lembrete ao paciente: 'Sua consulta foi agendada/atualizada. Verifique detalhes.'");
     }
